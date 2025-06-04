38cdb78f64c110f0f7c528bb0b3d6ad4a6751da0
tnqbao <quocbao.job106204@gmail.com>
2025-06-04 14:48:09 +0700

    refactor: profile api

--- providers/types.go
+++ providers/types.go
@@ -42,10 +42,14 @@ type UserPublic struct {
 }
 
 type UserInformationUpdateReq struct {
-	FullName    *string    `json:"fullname"`
-	Email       *string    `json:"email"`
-	Phone       *string    `json:"phone"`
-	DateOfBirth *time.Time `json:"date_of_birth"`
+	FullName    *string    `json:"fullname,omitempty"`
+	Username    *string    `json:"username,omitempty"`
+	Email       *string    `json:"email,omitempty"`
+	Phone       *string    `json:"phone,omitempty"`
+	Gender      *string    `json:"gender,omitempty"`
+	DateOfBirth *time.Time `json:"date_of_birth,omitempty"`
+	FacebookURL *string    `json:"facebook_url,omitempty"`
+	GitHubURL   *string    `json:"github_url,omitempty"`
 }
 type UserInformationUpdateRes struct {
 	UserId      uuid.UUID  `json:"user_id"`
