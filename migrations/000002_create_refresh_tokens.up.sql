bec22ecde4c34a74a297f97f60d393265b525370
tnqbao <quocbao.job106204@gmail.com>
2025-06-05 00:15:01 +0700

    refactor: update foreign key user_id

--- migrations/000002_create_refresh_tokens.up.sql
+++ migrations/000002_create_refresh_tokens.up.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE IF NOT EXISTS refresh_tokens (
                                               id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
     token TEXT NOT NULL,
     device_id TEXT,
     expires_at TIMESTAMP NOT NULL,
