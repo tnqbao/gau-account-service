61d5d33ab0d1d7258734e068805676e798f270b7
tnqbao <quocbao.job106204@gmail.com>
2025-06-05 15:55:35 +0700

    feat: add configuration for k8s

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Println("No .env file found, continuing with environment variables")
 	}
 
 	cf := config.LoadEnvConfig()
