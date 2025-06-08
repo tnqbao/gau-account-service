65f0af5cb321251b29f421e09d008d1a7e0d3eca
tnqbao <quocbao.job106204@gmail.com>
2025-06-08 19:17:56 +0700

    feat: add refresh token api

--- repositories/token.go
+++ repositories/token.go
@@ -1,9 +1,11 @@
 package repositories
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/tnqbao/gau-account-service/schemas"
 	"gorm.io/gorm"
+	"time"
 )
 
 func CreateRefreshToken(token *schemas.RefreshToken, c *gin.Context) error {
@@ -18,4 +20,26 @@ func DeleteRefreshTokenByTokenAndDevice(token string, deviceID string, c *gin.Co
 	db := c.MustGet("db").(*gorm.DB)
 	result := db.Where("token = ? AND device_id = ?", token, deviceID).Delete(&schemas.RefreshToken{})
 	return result.RowsAffected, result.Error
+}
+
+func GetUserInfoFromRefreshToken(token string, c *gin.Context) (*schemas.User, error) {
+	db := c.MustGet("db").(*gorm.DB)
+
+	var refreshToken schemas.RefreshToken
+	if err := db.Where("token = ?", token).First(&refreshToken).Error; err != nil {
+		return nil, err
+	}
+
+	if refreshToken.ExpiresAt.Before(time.Now()) {
+		return nil, fmt.Errorf("refresh token expired")
+	}
+
+	var user schemas.User
+	if err := db.Select("user_id, permission, fullname").
+		Where("user_id = ?", refreshToken.UserID).
+		First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
